0f786789bb524d4610a9b6a697ab43f1c7dfbfd0
OwO <43302188+FTC10641@users.noreply.github.com>
2019-11-02 15:04:57 -0500

    Add files via upload

--- SkyStone-5.2/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto/FirstAuto.java
+++ SkyStone-5.2/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto/FirstAuto.java
@@ -1,23 +1,18 @@
-/*
-This should just make the robot Drive Forward 10 inches
- */
-
 package org.firstinspires.ftc.teamcode.Auto;
 
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
+import org.firstinspires.ftc.teamcode.Odometry.*;
 import org.firstinspires.ftc.teamcode.SubSystems.*;
 
 @Autonomous (name = "FirstAuto")
 
 public class FirstAuto extends OpMode {
 
-    Motors motor = new Motors();
-    Actions act = new Actions();
-    Sensors sensor = new Sensors();
     ElapsedTime time = new ElapsedTime();
+
 
     enum State {
         DForward
@@ -27,9 +22,6 @@ public class FirstAuto extends OpMode {
 
     @Override   // Game initialized
     public void init(){
-        motor.initMotors(hardwareMap);
-        act.initActions(hardwareMap);
-        sensor.initSensors(hardwareMap);
         time = new ElapsedTime();
         driveState = State.DForward;
     }
@@ -45,13 +37,7 @@ public class FirstAuto extends OpMode {
         switch (driveState) {
 
             case DForward:
-                act.DForward(1,10);
-                if (act.DriveDone(10)){
-                    time.reset();
-                    act.Kill();
-                }
-                break;
-
+                telemetry.addLine("your dad str8");
         }
 
     }
