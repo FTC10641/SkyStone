64f7050786c98bfd8d4c6b868b68d7e30aaf2225
OwO <43302188+FTC10641@users.noreply.github.com>
2019-11-02 15:04:31 -0500

    Add files via upload

--- SkyStone-5.2/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/MechanumDrive.java
+++ SkyStone-5.2/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/MechanumDrive.java
@@ -1,12 +1,10 @@
-<<<<<<< HEAD
 /*
-    this is the java class for our TeleOp
+    this is the java class for our TestCode
  */
 
-=======
->>>>>>> parent of 17ae4d7... Odo stuff + editing stuff for new bot
 package org.firstinspires.ftc.teamcode.TeleOp;
 
+//import com.qualcomm.hardware.rev.RevBlinkinLedDriver;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.util.ElapsedTime;
@@ -15,30 +13,22 @@ import com.qualcomm.robotcore.util.Range;
 import org.firstinspires.ftc.teamcode.SubSystems.*;
 
 
-/**
- * This file contains an minimal example of a Linear "OpMode". An OpMode is a 'program' that runs in either
- * the autonomous or the teleop period of an FTC match. The names of OpModes appear on the menu
- * of the FTC Driver Station. When an selection is made from the menu, the corresponding OpMode
- * class is instantiated on the Robot Controller and executed.
- *
- * This particular OpMode just executes a Mechanum Drive Teleop for a fouro wheeled robot
- * It includes all the skeletal structure that all linear OpModes contain.
- *
- * Use Android Studios to Copy this Class, and Paste it into your team's code folder with a new name.
- * Remove or comment out the @Disabled line to add this opmode to the Driver Station OpMode list
- */
-
-@TeleOp(name="MechanumDrive", group="Linear Opmode")
+@TeleOp(name="TeleOp", group="Linear Opmode")
 public class MechanumDrive extends LinearOpMode {
 
-    Motors motor = new Motors();
+    Robot robot = new Robot();
     Sensors sensors = new Sensors();
 
     // Declare OpMode members.
     private ElapsedTime runtime = new ElapsedTime();
     private boolean changed = false;
+    private boolean changed2 = false;
 
     public float hsvValues[] = {0F, 0F, 0F};
+
+//    //LED Patterns
+//    RevBlinkinLedDriver.BlinkinPattern humanIndicator1;
+//    RevBlinkinLedDriver.BlinkinPattern off;
 
 
     @Override
@@ -46,8 +36,11 @@ public class MechanumDrive extends LinearOpMode {
         telemetry.addData("Status", "Initialized");
         telemetry.update();
 
-        motor.initMotors(hardwareMap);
+        robot.initRobot(hardwareMap);
         sensors.initSensors(hardwareMap);
+
+//        humanIndicator1 = RevBlinkinLedDriver.BlinkinPattern.YELLOW;
+//        off = RevBlinkinLedDriver.BlinkinPattern.BLACK;
 
         // Wait for the game to start (driver presses PLAY)
         waitForStart();
@@ -55,9 +48,8 @@ public class MechanumDrive extends LinearOpMode {
 
         // run until the end of the match (driver presses STOP)
         while (opModeIsActive()) {
-            sensors.ReadColor();
+            //sensors.ReadColor();
 
-<<<<<<< HEAD
             /*
             gamepad 1 controls, includes:
             Intake
@@ -65,26 +57,40 @@ public class MechanumDrive extends LinearOpMode {
             Drive Train
             Lift
              */
-//            if (RightTrigger()) {
-//                robot.rightIntake.setPower(-1);
-//                robot.leftIntake.setPower(1);
-//            } else if (LeftTrigger()) {
-//                robot.rightIntake.setPower(1);
-//                robot.leftIntake.setPower(-1);
-//            } else {
-//                robot.leftIntake.setPower(0);
-//                robot.rightIntake.setPower(0);
-//            }
+            if (RightTrigger()) {
+                robot.rightIntake.setPower(-1);
+                robot.leftIntake.setPower(1);
+            } else if (LeftTrigger()) {
+                robot.rightIntake.setPower(1);
+                robot.leftIntake.setPower(-1);
+            } else {
+                robot.leftIntake.setPower(0);
+                robot.rightIntake.setPower(0);
+            }
 
-//            if (gamepad1.y && !changed) {
-//                if (robot.sensorServo.getPosition() <= .3) {
-//                    robot.sensorServo.setPosition(.85);
-//                } else{
-//                    robot.sensorServo.setPosition(.2);
-//                }
-//                changed = true;
-//            } else if (!gamepad1.y) changed = false;
+            if (gamepad1.y && !changed) {
+                if (robot.sensorServo.getPosition() <= 0.8) {
+                    robot.sensorServo.setPosition(0.9);
+                } else{
+                    robot.sensorServo.setPosition(0.7);
+                }
+                changed = true;
+            } else if (!gamepad1.y) {
+                changed = false;
+            }
 
+            if (gamepad1.a && !changed2) {
+                if (robot.sensorServo.getPosition() <= 0.8) {
+                    robot.sensorServo.setPosition(0.9);
+                } else{
+                    robot.sensorServo.setPosition(0);
+                }
+                changed2 = true;
+            } else if (!gamepad1.a) {
+                changed2 = false;
+            }
+
+//
 //            if (gamepad1.y) {
 //                robot.Up(1,10);
 //            }
@@ -116,38 +122,10 @@ public class MechanumDrive extends LinearOpMode {
 //            if (sensors.red()) {
 //                telemetry.addLine("really, red, that's pretty rad ");
 //            }
-=======
-            if (RightTrigger()) {
-                motor.rightIntake.setPower(-1);
-                motor.leftIntake.setPower(1);
-            } else if (LeftTrigger()) {
-                motor.rightIntake.setPower(1);
-                motor.leftIntake.setPower(-1);
-            } else {
-                motor.leftIntake.setPower(0);
-                motor.rightIntake.setPower(0);
-            }
-
-            if (gamepad1.y && !changed) {
-                if (motor.sensorServo.getPosition() <= .3) {
-                    motor.sensorServo.setPosition(.85);
-                } else motor.sensorServo.setPosition(.2);
-                changed = true;
-            } else if (!gamepad1.y) changed = false;
-
-            if (sensors.blue()){
-                telemetry.addLine("bruh thats pretty blue");
-            }
-
-            if (sensors.red()) {
-                telemetry.addLine("really, red, thats pretty rad ");
-            }
-
->>>>>>> parent of 17ae4d7... Odo stuff + editing stuff for new bot
 
             // Setup a variable for each drive wheel to save power level for telemetry
-            double rightIntakePower;
-            double leftIntakePower;
+//            double rightIntakePower;
+//            double leftIntakePower;
             double frontRightPower;
             double backRightPower;
             double frontLeftPower;
@@ -155,20 +133,19 @@ public class MechanumDrive extends LinearOpMode {
 
             // POV Mode uses left stick to go forward, and right stick to turn.
             // - This uses basic math to combine motions and is easier to drive straight.
-            double strafe = gamepad1.left_stick_x;
+            double strafe = -gamepad1.left_stick_x;
             double drive = gamepad1.left_stick_y;
             double turn = gamepad1.right_stick_x;
-            frontLeftPower = Range.clip(drive + turn + strafe, -1.0, 1.0);
-            backLeftPower = Range.clip(drive + turn - strafe, -1.0, 1.0);
+            frontLeftPower = Range.clip(drive + turn - strafe, -1.0, 1.0);
+            backLeftPower = Range.clip(drive + turn + strafe, -1.0, 1.0);
             backRightPower = Range.clip(drive - turn + strafe, -1.0, 1.0);
             frontRightPower = Range.clip(drive - turn - strafe, -1.0, 1.0);
-            rightIntakePower = Range.clip(-gamepad1.right_trigger + gamepad1.left_trigger,-1.0,1.0); //controls for the Intake motors
-            leftIntakePower = Range.clip(-gamepad1.right_trigger + gamepad1.left_trigger, -1.0, 1.0);
+//            rightIntakePower = Range.clip(-gamepad1.right_trigger + gamepad1.left_trigger,-1.0,1.0); //controls for the Intake motors
+//            leftIntakePower = Range.clip(-gamepad1.right_trigger + gamepad1.left_trigger, -1.0, 1.0);
+//
 
-
-<<<<<<< HEAD
-            rightIntakePower = (float) scaleInput(rightIntakePower);
-            leftIntakePower = (float) scaleInput(leftIntakePower);
+//            rightIntakePower = (float) scaleInput(rightIntakePower);
+//            leftIntakePower = (float) scaleInput(leftIntakePower);
             frontLeftPower = (float) scaleInput(frontLeftPower);
             backLeftPower = (float) scaleInput(backLeftPower);
             frontRightPower = (float) scaleInput(frontRightPower);
@@ -178,35 +155,60 @@ public class MechanumDrive extends LinearOpMode {
             robot.backLeft.setPower(backLeftPower);
             robot.frontRight.setPower(frontRightPower);
             robot.backRight.setPower(backRightPower);
-            robot.rightIntake.setPower(rightIntakePower);
-            robot.leftIntake.setPower(leftIntakePower);
-            
-=======
-            // Send calculated power to wheels
-            motor.frontLeft.setPower(frontLeftPower);
-            motor.backLeft.setPower(backLeftPower);
-            motor.frontRight.setPower(frontRightPower);
-            motor.backRight.setPower(backRightPower);
->>>>>>> parent of 17ae4d7... Odo stuff + editing stuff for new bot
+//            robot.rightIntake.setPower(rightIntakePower);
+//            robot.leftIntake.setPower(leftIntakePower);
+
 
 
             // Show the elapsed game time and wheel power.
             telemetry.addData("Status", "Run Time: " + runtime.toString());
-            telemetry.addData("Motors", "left (%.2f), right (%.2f)",
+            telemetry.addData("Robot", "left (%.2f), right (%.2f)",
                     backLeftPower, frontLeftPower, frontRightPower, backRightPower);
             telemetry.addData("hue", hsvValues[0]);
             telemetry.update();
         }
     }
 
+    /** This Scale Array function establishes a set of increasing values
+     that is used to control the speed of motors and servos. It allows them to exponentially increases their rotation.  **/
+
+    public double scaleInput(double dVal)  {
+        double[] scaleArray = { 0.0, 0.05, 0.09, 0.10, 0.12, 0.15, 0.18, 0.24,
+                0.30, 0.36, 0.43, 0.50, 0.60, 0.72, 0.85, 1.00, 1.00 };
+
+        // get the corresponding index for the scaleInput array.
+        int index = (int) (dVal * 16.0);
+
+        // index should be positive.
+        if (index < 0) {
+            index = -index;
+        }
+
+        // index cannot exceed size of array minus 1.
+        if (index > 16) {
+            index = 16;
+        }
+
+        // get value from the array.
+        double dScale = 0.0;
+        if (dVal < 0) {
+            dScale = -scaleArray[index];
+        } else {
+            dScale = scaleArray[index];
+        }
+
+        // return scaled value.
+        return dScale;
+    }
+
     //so trigger grazing doesn't activate the intake
-//    boolean RightTrigger() {
-//        if (gamepad1.right_trigger >= 0.1) {
-//            return(true); }
-//        else return(false); }
-//
-//    boolean LeftTrigger() {
-//        if (gamepad1.left_trigger >= 0.1) {
-//            return(true); }
-//        else return(false); }
+    boolean RightTrigger() {
+        if (gamepad1.right_trigger >= 0.1) {
+            return(true); }
+        else return(false); }
+
+    boolean LeftTrigger() {
+        if (gamepad1.left_trigger >= 0.1) {
+            return(true); }
+        else return(false); }
 }
