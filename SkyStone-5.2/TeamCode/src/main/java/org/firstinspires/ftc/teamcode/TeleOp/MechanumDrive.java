2911ab48dbe03a1369fec1f50ea16b0c8c6de4d2
OwO <43302188+FTC10641@users.noreply.github.com>
2019-12-13 14:01:18 -0600

    Add files via upload

--- SkyStone-5.2/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/MechanumDrive.java
+++ SkyStone-5.2/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/MechanumDrive.java
@@ -1,35 +1,42 @@
 /*
-    this is the java class for our TestCode
+    This is the java class for our Tele op
  */
 
 package org.firstinspires.ftc.teamcode.TeleOp;
 
-//import com.qualcomm.hardware.rev.RevBlinkinLedDriver;
+
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.util.ElapsedTime;
 import com.qualcomm.robotcore.util.Range;
 
-import org.firstinspires.ftc.teamcode.SubSystems.*;
-
+import org.firstinspires.ftc.teamcode.SubSystems.Robot;
+import org.firstinspires.ftc.teamcode.SubSystems.Sensors;
 
 @TeleOp(name="TeleOp", group="Linear Opmode")
 public class MechanumDrive extends LinearOpMode {
 
+    //Gives this code access to the Hardware Maps
     Robot robot = new Robot();
     Sensors sensors = new Sensors();
 
     // Declare OpMode members.
     private ElapsedTime runtime = new ElapsedTime();
-    private boolean changed = false;
-    private boolean changed2 = false;
+    private boolean toggle = true;
+    private boolean toggle2 = false;
 
-    public float hsvValues[] = {0F, 0F, 0F};
 
-//    //LED Patterns
-//    RevBlinkinLedDriver.BlinkinPattern humanIndicator1;
-//    RevBlinkinLedDriver.BlinkinPattern off;
+//    lift variables
+    private double maxLiftHeight = 5;
+    private double minLiftHeight = 0;
 
+
+    private int maxCarriage = 19;
+    private int minCarriage = 0;
+    private boolean carriageToggle = false;
+    private boolean carriageOn = false;
+    private int carriagePosition = 0;
 
     @Override
     public void runOpMode() {
@@ -39,93 +46,95 @@ public class MechanumDrive extends LinearOpMode {
         robot.initRobot(hardwareMap);
         sensors.initSensors(hardwareMap);
 
-//        humanIndicator1 = RevBlinkinLedDriver.BlinkinPattern.YELLOW;
-//        off = RevBlinkinLedDriver.BlinkinPattern.BLACK;
-
-        // Wait for the game to start (driver presses PLAY)
         waitForStart();
         runtime.reset();
 
         // run until the end of the match (driver presses STOP)
         while (opModeIsActive()) {
-            //sensors.ReadColor();
 
             /*
-            gamepad 1 controls, includes:
-            Intake
-            the servo that is connected to the color sensor
-            Drive Train
-            Lift
+            Gamepad 1 controls include:
+                Intake
+                Block Grabber
+                Drive Train
+                Lift
              */
-            if (RightTrigger()) {
-                robot.rightIntake.setPower(-1);
-                robot.leftIntake.setPower(1);
-            } else if (LeftTrigger()) {
+
+            //Intake
+            if (gamepad1.right_bumper) {
                 robot.rightIntake.setPower(1);
                 robot.leftIntake.setPower(-1);
-            } else {
+            }
+            else if (gamepad1.left_bumper) {
+                robot.rightIntake.setPower(-1);
+                robot.leftIntake.setPower(1);
+            }
+            else {
                 robot.leftIntake.setPower(0);
                 robot.rightIntake.setPower(0);
             }
 
-            if (gamepad1.y && !changed) {
-                if (robot.sensorServo.getPosition() <= 0.8) {
-                    robot.sensorServo.setPosition(0.9);
-                } else{
-                    robot.sensorServo.setPosition(0.7);
-                }
-                changed = true;
-            } else if (!gamepad1.y) {
-                changed = false;
+            //block grabber this works by checking if y button is pressed and if toggle is false
+            if (gamepad1.y) {
+                robot.blockGrabber.setPosition(1);
+            }
+            else if (gamepad1.a) {
+                robot.blockGrabber.setPosition(0);
+            }
+            else if (gamepad1.x) {
+                robot.blockGrabber.setPosition(.25);
             }
 
-            if (gamepad1.a && !changed2) {
-                if (robot.sensorServo.getPosition() <= 0.8) {
-                    robot.sensorServo.setPosition(0.9);
-                } else{
-                    robot.sensorServo.setPosition(0);
+            //Carriage Control
+            if (gamepad1.dpad_right && !carriageToggle){
+                if (carriageOn)
+                    robot.carriage.setTargetPosition(carriagePosition = maxCarriage);
+                else {
+                    robot.carriage.setTargetPosition(carriagePosition = minCarriage);
                 }
-                changed2 = true;
-            } else if (!gamepad1.a) {
-                changed2 = false;
+                carriageOn = !carriageOn;
+                carriageToggle = true;
+            }
+            else if (!gamepad1.dpad_right){
+                carriageToggle = false;
+            }
+            if (robot.carriage.getCurrentPosition() != carriagePosition * (robot.CarriageCountsPerInch)) {
+                robot.carriage.setTargetPosition((int) (carriagePosition * (robot.CarriageCountsPerInch)));
+                robot.carriage.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+                robot.carriage.setPower(1);
             }
 
-//
-//            if (gamepad1.y) {
-//                robot.Up(1,10);
-//            }
-//            else if (gamepad1.a){
-//                robot.Down(1,10);
-//            }
-//            else {
-//                robot.lift.setPower(0);
-//                robot.Kill();
-//            }
-
-             /*
-            gamepad 2 controls, includes:
-            LED indicators
+            /*
+            Gamepad 2 controls include:
+                Foundation Servos
+                Indexer Servo
              */
-//             if (gamepad2.y){
-//                 robot.blinkinLedDriver.setPattern(humanIndicator1);
-//                 telemetry.addLine("Bot Hungry Boi");
-//             }
-//             else {
-//                 robot.blinkinLedDriver.setPattern(off);
-//             }
 
+            //foundation grabber
+            if (toggle && gamepad2.a) {  // Only execute once per Button push
+                toggle = false;  // Prevents this section of code from being called again until the Button is released and re-pressed
+                if (toggle2) {
+                    toggle2 = false;
+                    robot.foundationServo1.setPosition(0);
+                    robot.foundationServo2.setPosition(1);
+                }
+                else {
+                    toggle2 = true;
+                    robot.foundationServo1.setPosition(1);
+                    robot.foundationServo2.setPosition(0);
+                }
+            } else if (!gamepad2.a) {
+                toggle = true; // Button has been released, so this allows a re-press to activate the code above.
+            }
 
-//            if (sensors.blue()){
-//                telemetry.addLine("bruh that's pretty blue");
-//            }
-//
-//            if (sensors.red()) {
-//                telemetry.addLine("really, red, that's pretty rad ");
-//            }
+            if (gamepad2.x) {
+                robot.indexer.setPosition(1);
+            }
+            else {
+                robot.indexer.setPosition(.5);
+            }
 
-            // Setup a variable for each drive wheel to save power level for telemetry
-//            double rightIntakePower;
-//            double leftIntakePower;
+            double liftPower;
             double frontRightPower;
             double backRightPower;
             double frontLeftPower;
@@ -133,82 +142,31 @@ public class MechanumDrive extends LinearOpMode {
 
             // POV Mode uses left stick to go forward, and right stick to turn.
             // - This uses basic math to combine motions and is easier to drive straight.
-            double strafe = -gamepad1.left_stick_x;
+            double strafe = gamepad1.left_stick_x;
             double drive = gamepad1.left_stick_y;
-            double turn = gamepad1.right_stick_x;
+            double turn = -gamepad1.right_stick_x;
+
             frontLeftPower = Range.clip(drive + turn - strafe, -1.0, 1.0);
             backLeftPower = Range.clip(drive + turn + strafe, -1.0, 1.0);
-            backRightPower = Range.clip(drive - turn + strafe, -1.0, 1.0);
-            frontRightPower = Range.clip(drive - turn - strafe, -1.0, 1.0);
-//            rightIntakePower = Range.clip(-gamepad1.right_trigger + gamepad1.left_trigger,-1.0,1.0); //controls for the Intake motors
-//            leftIntakePower = Range.clip(-gamepad1.right_trigger + gamepad1.left_trigger, -1.0, 1.0);
-//
+            backRightPower = Range.clip(drive - turn - strafe, -1.0, 1.0);
+            frontRightPower = Range.clip(drive - turn + strafe, -1.0, 1.0);
 
-//            rightIntakePower = (float) scaleInput(rightIntakePower);
-//            leftIntakePower = (float) scaleInput(leftIntakePower);
-            frontLeftPower = (float) scaleInput(frontLeftPower);
-            backLeftPower = (float) scaleInput(backLeftPower);
-            frontRightPower = (float) scaleInput(frontRightPower);
-            backRightPower =  (float) scaleInput(backRightPower);
-            // Send calculated power to wheels and intake motors
-            robot.frontLeft.setPower(frontLeftPower);
-            robot.backLeft.setPower(backLeftPower);
-            robot.frontRight.setPower(frontRightPower);
-            robot.backRight.setPower(backRightPower);
-//            robot.rightIntake.setPower(rightIntakePower);
-//            robot.leftIntake.setPower(leftIntakePower);
+            liftPower = Range.clip(gamepad1.right_trigger - gamepad1.left_trigger, -1.0, 1.0);
 
+            //Drive Controls, Adds a AntiTurbo Button else it will drive full speed
+            robot.frontLeft.setPower(gamepad2.y ? frontLeftPower/2 : frontLeftPower);
+            robot.backLeft.setPower(gamepad2.y ? backLeftPower/2 : backLeftPower);
+            robot.frontRight.setPower(gamepad2.y ? frontRightPower/2 : frontRightPower);
+            robot.backRight.setPower(gamepad2.y ? backRightPower/2 : backRightPower);
 
+            robot.lift.setPower(liftPower);
 
             // Show the elapsed game time and wheel power.
             telemetry.addData("Status", "Run Time: " + runtime.toString());
+            telemetry.addData("Lift", robot.lift.getCurrentPosition());
             telemetry.addData("Robot", "left (%.2f), right (%.2f)",
                     backLeftPower, frontLeftPower, frontRightPower, backRightPower);
-            telemetry.addData("hue", hsvValues[0]);
             telemetry.update();
         }
     }
-
-    /** This Scale Array function establishes a set of increasing values
-     that is used to control the speed of motors and servos. It allows them to exponentially increases their rotation.  **/
-
-    public double scaleInput(double dVal)  {
-        double[] scaleArray = { 0.0, 0.05, 0.09, 0.10, 0.12, 0.15, 0.18, 0.24,
-                0.30, 0.36, 0.43, 0.50, 0.60, 0.72, 0.85, 1.00, 1.00 };
-
-        // get the corresponding index for the scaleInput array.
-        int index = (int) (dVal * 16.0);
-
-        // index should be positive.
-        if (index < 0) {
-            index = -index;
-        }
-
-        // index cannot exceed size of array minus 1.
-        if (index > 16) {
-            index = 16;
-        }
-
-        // get value from the array.
-        double dScale = 0.0;
-        if (dVal < 0) {
-            dScale = -scaleArray[index];
-        } else {
-            dScale = scaleArray[index];
-        }
-
-        // return scaled value.
-        return dScale;
-    }
-
-    //so trigger grazing doesn't activate the intake
-    boolean RightTrigger() {
-        if (gamepad1.right_trigger >= 0.1) {
-            return(true); }
-        else return(false); }
-
-    boolean LeftTrigger() {
-        if (gamepad1.left_trigger >= 0.1) {
-            return(true); }
-        else return(false); }
 }
