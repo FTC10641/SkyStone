cdad86ccc9671762f40a212ac0054ad5de72dfc6
OwO <43302188+FTC10641@users.noreply.github.com>
2019-12-13 13:59:51 -0600

    Add files via upload

--- SkyStone-5.2/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SubSystems/Robot.java
+++ SkyStone-5.2/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SubSystems/Robot.java
@@ -6,9 +6,10 @@ package org.firstinspires.ftc.teamcode.SubSystems;
 
 //import com.qualcomm.hardware.rev.RevBlinkinLedDriver;
 import com.qualcomm.robotcore.hardware.DcMotor;
-import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.DigitalChannel;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.util.Range;
 
 public class Robot {
 
@@ -20,24 +21,53 @@ public class Robot {
     public DcMotor rightIntake = null;
     public DcMotor leftIntake = null;
     public DcMotor lift = null;
+    public DcMotor carriage = null;
+
+    public DcMotor verticalLeft;
+    public DcMotor verticalRight;
+    public DcMotor horizontal;
+
 
     //setting up variables for servos
-    public Servo sensorServo = null;
     public Servo blockGrabber = null;
+    public Servo foundationServo1 =  null;
+    public Servo foundationServo2 = null;
+    public Servo indexer = null;
 
-    //REV blinkin for the LEDs
-//    public RevBlinkinLedDriver blinkinLedDriver = null;
+    //variables for the limit switches
+    public DigitalChannel insideLimit = null;
+    public DigitalChannel outsideLimit = null;
 
+    double liftPower;
+    double frontRightPower;
+    double backRightPower;
+    double frontLeftPower;
+    double backLeftPower;
 
-    //most important part of the code:
-    //;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+    /*
+    Lift Encoder Information
+     */
+    public final double COUNTS_PER_MOTOR_REV =  537.6;    // eg: Andy Mark Motor Encoder
+    public final double DRIVE_GEAR_REDUCTION = 1;
+    public final double PINION_DIAMETER_INCHES = 1.10236;     // For figuring circumference
+    public final double LiftCountsPerInch = (COUNTS_PER_MOTOR_REV * DRIVE_GEAR_REDUCTION) /
+            (PINION_DIAMETER_INCHES * 3.1415);
 
-    //information for the encoders in the motor
-    public static final double COUNTS_PER_MOTOR_REV =  537.6;    // eg: Andy Mark Motor Encoder
-    public static final double DRIVE_GEAR_REDUCTION = 1;
-    public static final double WHEEL_DIAMETER_INCHES = 1.10236;     // For figuring circumference
-    public static final double COUNTS_PER_INCH = (COUNTS_PER_MOTOR_REV * DRIVE_GEAR_REDUCTION) /
-            (WHEEL_DIAMETER_INCHES * 3.1415);
+    /*
+    Carriage Encoder information
+     */
+    public static final double COUNTS_PER_MOTOR_REV1 =  103.6;
+    public static final double DRIVE_GEAR_REDUCTION1 = 1;
+    public static final double CarriageCountsPerInch = (COUNTS_PER_MOTOR_REV1 * DRIVE_GEAR_REDUCTION1) /
+            (3.1415);
+
+    /*
+    Odometry Encoder information
+     */
+    public final double COUNTS_PER_MOTOR_REV2 =  360;
+    public final double DRIVE_GEAR_REDUCTION2 = 2;
+    public final double WHEEL_DIAMETER_INCHES2 = 4;     // For figuring circumference
+    public final double OdometryCountsPerInch = (COUNTS_PER_MOTOR_REV2 * DRIVE_GEAR_REDUCTION2) / (WHEEL_DIAMETER_INCHES2 * 3.1415);
 
     HardwareMap hwMap = null;
 
@@ -53,14 +83,32 @@ public class Robot {
         frontRight = ahwMap.get(DcMotor.class, "rightF");
         frontLeft = ahwMap.get(DcMotor.class, "leftF");
 
+        //odometry wheels
+        String backR = "rightB";
+        String backL = "leftB";
+        String frontR = "rightF";
+        String frontL = "leftF";
+
+        String verticalLeftEncoderName = frontR;
+        String verticalRightEncoderName = frontL;
+        String horizontalEncoderName = backL;
+
+        verticalLeft = ahwMap.dcMotor.get(verticalLeftEncoderName);
+        verticalRight = ahwMap.dcMotor.get(verticalRightEncoderName);
+        horizontal = ahwMap.dcMotor.get(horizontalEncoderName);
+
         rightIntake = ahwMap.get(DcMotor.class,"rIntake");
         leftIntake = ahwMap.get(DcMotor.class,"lIntake");
         lift = ahwMap.get(DcMotor.class,"lift");
+        carriage = ahwMap.get(DcMotor.class,"carriage");
 
-        sensorServo = ahwMap.get(Servo.class, "csServo");
-        blockGrabber= ahwMap.get(Servo.class, "grabber");
+        blockGrabber = ahwMap.get(Servo.class, "grabber");
+        foundationServo1 = ahwMap.get(Servo.class, "found1");
+        foundationServo2 = ahwMap.get(Servo.class, "found2");
+        indexer = ahwMap.get(Servo.class, "index");
 
-//        blinkinLedDriver = ahwMap.get(RevBlinkinLedDriver.class, "blinkin");
+        insideLimit = ahwMap.get(DigitalChannel.class, "inside");
+        outsideLimit = ahwMap.get(DigitalChannel.class, "outside");
 
         backRight.setPower(0);
         backLeft.setPower(0);
@@ -77,6 +125,26 @@ public class Robot {
 
         lift.setDirection(DcMotor.Direction.REVERSE);
 
+        frontRight.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        backRight.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        frontLeft.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        backLeft.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+
+        frontRight.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        backRight.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        frontLeft.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        backLeft.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+
+        verticalLeft.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        verticalRight.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        horizontal.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+
+        verticalLeft.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        verticalRight.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        horizontal.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+
+        carriage.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+
         // adding brakes to the motor so they immediately stop when told
         backRight.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         backLeft.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
@@ -87,45 +155,176 @@ public class Robot {
         rightIntake.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
 
         lift.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        carriage.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
 
-
+        frontLeftPower = (float) scaleInput(frontLeftPower);
+        backLeftPower = (float) scaleInput(backLeftPower);
+        frontRightPower = (float) scaleInput(frontRightPower);
+        backRightPower = (float) scaleInput(backRightPower);
+        liftPower = (float) scaleInput(liftPower);
     }
-    public void DForward (double speed, double distance){
-        backLeft.setTargetPosition((int) (-distance * COUNTS_PER_INCH));
-        frontLeft.setTargetPosition((int) (-distance * COUNTS_PER_INCH));
-        backRight.setTargetPosition((int) (-distance * COUNTS_PER_INCH));
-        frontRight.setTargetPosition((int) (-distance * COUNTS_PER_INCH));
 
-        backLeft.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-        frontLeft.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-        backRight.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-        frontRight.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+    /**
+     * This Scale Array function establishes a set of increasing values
+     * that is used to control the speed of motors and servos. It allows them to exponentially increases their rotation.
+     **/
+    public double scaleInput(double dVal) {
+        double[] scaleArray = {0.0, 0.05, 0.09, 0.10, 0.12, 0.15, 0.18, 0.24,
+                0.30, 0.36, 0.43, 0.50, 0.60, 0.72, 0.85, 1.00, 1.00};
 
-        backLeft.setPower(speed);
-        frontLeft.setPower(speed);
-        backRight.setPower(speed);
-        frontRight.setPower(speed);
-    }
-    public void Up(double speed, double distance){
-        lift.setTargetPosition((int) (distance * COUNTS_PER_INCH));
+        // get the corresponding index for the scaleInput array.
+        int index = (int) (dVal * 16.0);
 
-        lift.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-
-        lift.setPower(speed);
+        // index should be positive.
+        if (index < 0) {
+            index = -index;
         }
-    public void Down(double speed, double distance){
-        lift.setTargetPosition((int) (distance * COUNTS_PER_INCH));
 
-        lift.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        // index cannot exceed size of array minus 1.
+        if (index > 16) {
+            index = 16;
+        }
 
-        lift.setPower(-speed);
+        // get value from the array.
+        double dScale = 0.0;
+        if (dVal < 0) {
+            dScale = -scaleArray[index];
+        } else {
+            dScale = scaleArray[index];
+        }
+
+        // return scaled value.
+        return dScale;
     }
+    public void Forward(double speed, double distance){
+        frontRight.setTargetPosition((int) (distance * OdometryCountsPerInch));
+        backRight.setTargetPosition((int) (distance * OdometryCountsPerInch));
+        frontLeft.setTargetPosition((int) (distance * OdometryCountsPerInch));
+        backLeft.setTargetPosition((int) (distance * OdometryCountsPerInch));
+
+        frontRight.setPower(-speed);
+        backRight.setPower(-speed);
+        frontLeft.setPower(-speed);
+        backLeft.setPower(-speed);
+    }
+
+    public void Reverse(double speed, double distance){
+        frontRight.setTargetPosition((int) (distance * OdometryCountsPerInch));
+        backRight.setTargetPosition((int) (distance * OdometryCountsPerInch));
+        frontLeft.setTargetPosition((int) (distance * OdometryCountsPerInch));
+        backLeft.setTargetPosition((int) (distance * OdometryCountsPerInch));
+
+        frontRight.setPower(speed);
+        backRight.setPower(speed);
+        frontLeft.setPower(speed);
+        backLeft.setPower(speed);
+    }
+
+    public void StrafeLeft(double speed, double distance){
+        frontRight.setTargetPosition((int) (distance * OdometryCountsPerInch));
+        backRight.setTargetPosition((int) (distance * OdometryCountsPerInch));
+        frontLeft.setTargetPosition((int) (distance * OdometryCountsPerInch));
+        backLeft.setTargetPosition((int) (distance * OdometryCountsPerInch));
+
+        frontRight.setPower(-speed);
+        backRight.setPower(speed);
+        frontLeft.setPower(speed);
+        backLeft.setPower(-speed);
+    }
+
+    public void StrafeRight(double speed, double distance){
+        frontRight.setTargetPosition((int) (distance * OdometryCountsPerInch));
+        backRight.setTargetPosition((int) (distance * OdometryCountsPerInch));
+        frontLeft.setTargetPosition((int) (distance * OdometryCountsPerInch));
+        backLeft.setTargetPosition((int) (distance * OdometryCountsPerInch));
+
+        frontRight.setPower(speed);
+        backRight.setPower(-speed);
+        frontLeft.setPower(-speed);
+        backLeft.setPower(speed);
+    }
+
+    public void TurnAbsolute(double target, double heading){
+
+//        frontLeft.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+//        backLeft.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+//        frontRight.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+//        backRight.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+
+        verticalLeft.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        verticalRight.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        horizontal.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+
+
+        double Error = heading - target;
+        double Kp = 0.017;
+        double LFPower;
+        double LRPower;
+        double RFPower;
+        double RRPower;
+        double EPower;
+
+        if ((Math.abs(Error)) > 2 ){
+            LFPower = -Error * Kp;
+            LRPower = -Error * Kp;
+            RFPower = Error * Kp;
+            RRPower = Error * Kp;
+            EPower = Error * Kp;
+
+            Range.clip(LFPower,-1,1);
+            Range.clip(LRPower,-1,1);
+            Range.clip(RFPower,-1,1);
+            Range.clip(RRPower,-1,1);
+            Range.clip(EPower,-1,1);
+
+            frontLeft.setPower(LFPower);
+            backLeft.setPower(LRPower);
+            frontRight.setPower(RFPower);
+            backRight.setPower(RRPower);
+        }
+        else {
+            frontLeft.setPower(0);
+            backLeft.setPower(0);
+            frontRight.setPower(0);
+            backRight.setPower(0);
+        }
+    }
+
+    public void StopUsingEncoder(){
+        frontRight.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        backRight.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        frontLeft.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        backLeft.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+
+        verticalLeft.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        verticalRight.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        horizontal.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+
+    }
+    
     public void Kill(){
         backLeft.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
         frontLeft.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
         backRight.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
         frontRight.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        verticalLeft.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        verticalRight.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        horizontal.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+
         lift.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        carriage.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+    }
+
+    public void LiftDown(){
+        blockGrabber.setPosition(.25);
+        if (insideLimit.getState()){
+            lift.setPower(.9);
+        }
+    }
+
+    public void FoundationGrab(){
+        foundationServo1.setPosition(1);
+        foundationServo2.setPosition(0);
     }
 
 
@@ -141,8 +340,9 @@ public class Robot {
     }
 
     public boolean DriveDone(double distance){
-        if (Math.abs(frontLeft.getCurrentPosition() / COUNTS_PER_INCH) >= distance
-                || Math.abs(backRight.getCurrentPosition() / COUNTS_PER_INCH) >= distance)
+        if (Math.abs(frontLeft.getCurrentPosition() / OdometryCountsPerInch) >= distance
+                || Math.abs(frontRight.getCurrentPosition() / OdometryCountsPerInch) >= distance
+                || Math.abs(backLeft.getCurrentPosition() / OdometryCountsPerInch) >= distance)
         {
             return (true);
         } else return (false);
--- SkyStone-5.2/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SubSystems/Sensors.java
+++ SkyStone-5.2/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SubSystems/Sensors.java
@@ -4,28 +4,20 @@ This is where all of the robot's sensors live and where we init them and declare
 
 package org.firstinspires.ftc.teamcode.SubSystems;
 
-import android.graphics.Color;
-
 import com.qualcomm.hardware.bosch.BNO055IMU;
 import com.qualcomm.hardware.bosch.JustLoggingAccelerationIntegrator;
-import com.qualcomm.robotcore.hardware.ColorSensor;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 
+import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
 import org.firstinspires.ftc.robotcore.external.navigation.Orientation;
 
 public class Sensors extends Robot {
 
     public BNO055IMU imu;
 
-//    public ColorSensor colorSensor = null;
-//
-//    public float hsvValues[] = {0F, 0F, 0F};
-//
-//    public final float values[] = hsvValues;
-//
-//    public final double SCALE_FACTOR = 255;
-
     public Orientation angles;
+    WebcamName webcamName = null;
+
 
     HardwareMap hwMap = null;
 
@@ -33,9 +25,8 @@ public class Sensors extends Robot {
     public void initSensors(HardwareMap ahwMap) {
         hwMap = ahwMap;
 
-        //colorSensor = ahwMap.get(ColorSensor.class, "colorS");
-
         imu = hwMap.get(BNO055IMU.class, "imu");
+        webcamName = ahwMap.get(WebcamName.class, "Webcam 1");
 
         BNO055IMU.Parameters parameters = new BNO055IMU.Parameters();
         parameters.angleUnit = BNO055IMU.AngleUnit.DEGREES;
@@ -48,24 +39,3 @@ public class Sensors extends Robot {
 
     }
 }
-//    public void ReadColor(){
-//        // convert the RGB values to HSV values.
-//        // multiply by the SCALE_FACTOR.
-//        // then cast it back to int (SCALE_FACTOR is a double)
-//        Color.RGBToHSV((int) (colorSensor.red() * SCALE_FACTOR),
-//                (int) (colorSensor.green() * SCALE_FACTOR),
-//                (int) (colorSensor.blue() * SCALE_FACTOR),
-//                hsvValues);
-//    }
-//
-//    public boolean red(){
-//        if (hsvValues[0] >= 340 && hsvValues[0] <= 370 || hsvValues[0] >= 0 && hsvValues[0] <= 40){
-//            return(true);}
-//        else return(false);
-//    }
-//    public boolean blue(){
-//        if (hsvValues[0] >= 190 && hsvValues[0] <= 300){
-//            return(true);}
-//        else return(false);
-//    }
-//}
